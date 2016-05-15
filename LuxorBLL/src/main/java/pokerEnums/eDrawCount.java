d6f3210512abcb87546284b74ca02fa9c37d2821
Linghan Kong <konglhan@udel.edu>
2016-05-16 07:55:41 +0800

    Lab6

---
+++ LuxorBLL/src/main/java/pokerEnums/eDrawCount.java
@@ -0,0 +1,24 @@
+package pokerEnums;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public enum eDrawCount {
+
+	FIRST(1), SECOND(2), THIRD(3), FOURTH(4), FIFTH(5), SIXTH(6), SEVENTH(7), EIGHTH(8), NINTH(9), TENTH(10);
+	
+	private eDrawCount(final int DrawNo){
+		this.DrawNo = DrawNo;
+	}
+
+	private int DrawNo;
+	
+	public int getDrawNo(){
+		return DrawNo;
+	}
+	private static Map<Integer, eDrawCount> map = new HashMap<Integer, eDrawCount>();
+	
+	 public static eDrawCount getDrawNo(int drawNo) {
+	        return map.get(drawNo);
+	    }
+}
